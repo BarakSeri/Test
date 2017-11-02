54adb906c91ae79b90d50ca0e9dba666850a6a0a
Barak Seri <barak.seri@Kaltura.com>
2017-11-02 16:26:54 +0100

    hello

--- script1.sh
+++ script1.sh
@@ -4,5 +4,5 @@ git add file1.txt
 git status
 git commit file1.txt -m 'bla bla'
 git status
-git push file1.txt
+git push 
 git status
---
+++ script2.sh
@@ -0,0 +1,6 @@
+echo Please enter your sciprt 
+read input
+echo $input > scriptText.sh
+git add scriptText.sh
+git commit -m 'Shalom'
+git push
